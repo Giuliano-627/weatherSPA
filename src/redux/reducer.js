aa0c6859a8899b9a578030ffbe526e30e69feb69
Giuliano-627 <f15ur4.97@gmail.com>
2022-12-16 19:38:32 -0300

    Fixed insignificant zeros

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,19 +7,16 @@ const initialState = {
 function rootReducer(state = initialState, action) {
   switch (action.type) {
     case "DATA_WEATHER":
-      console.log("Entrando en data_weather");
       return {
         ...state,
         datos: action.payload,
       };
     case "POSIBLE_CITIES":
-      console.log("Entrando en POSIBLES CIUDADES");
       return {
         ...state,
         posiblesCiudades: action.payload,
       };
     default:
-      console.log("DEFAULT SWITCH REDUCER");
       return {
         ...state,
       };
